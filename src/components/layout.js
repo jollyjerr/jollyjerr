6934f92a9c403f691a0e6a56ed37feefc0afb467
jollyjerr <jollyjerr@gmail.com>
2021-04-04 07:29:23 -0600

    store dark mode state in local storage

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,22 +3,19 @@ import MobileFooter from "./MobileFooter";
 import Navbar from "./navbar";
 import PropTypes from "prop-types";
 import React from "react";
-
-export const DarkModeContext = React.createContext(false);
+import useLocalStorage from "./useLocalStorage";
 
 const Layout = ({ children }) => {
-  const [darkMode, setDarkMode] = React.useState(false); // TODO: get initial value from local storage
+  const [darkMode, setDarkMode] = useLocalStorage("jtabb.dev:DarkMode", false);
   return (
-    <DarkModeContext.Provider value={darkMode}>
-      <div className={darkMode ? "dark" : ""}>
-        <div className="bg-gray-100 dark:bg-gray-800 font-Raleway min-h-screen overflow-x-hidden">
-          <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-          <main className="container mx-auto">{children}</main>
-          <Footer />
-          <MobileFooter />
-        </div>
+    <div className={darkMode ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-800 font-Raleway min-h-screen overflow-x-hidden">
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+        <main className="container mx-auto">{children}</main>
+        <Footer />
+        <MobileFooter />
       </div>
-    </DarkModeContext.Provider>
+    </div>
   );
 };
 
