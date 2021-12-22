792791c70b838bdc23112381386712d1f88fdb5e
Jeremiah Tabb <jollyjerr@gmail.com>
2021-12-21 17:48:54 -0700

    get tailwind working

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/settings">Settings</a>
+</nav>
+
+<main class="min-h-screen bg-gray-700">
+  <slot />
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
