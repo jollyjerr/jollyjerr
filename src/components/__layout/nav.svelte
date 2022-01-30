be02bb4414024d8ab325271b8bf17831cf3c8d3e
Jeremiah Tabb <jollyjerr@gmail.com>
2022-01-30 14:11:58 -0700

    theme baby

--- src/components/__layout/nav.svelte
+++ src/components/__layout/nav.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <nav
-  class="fixed z-20 flex w-full items-center justify-center border-b border-gray-800 border-opacity-50 bg-gray-900 bg-opacity-95 py-8 lg:py-4"
+  class="fixed z-20 flex w-full items-center justify-center border-b border-gray-800 border-opacity-50 dark:bg-gray-900 bg-white bg-opacity-95 py-8 lg:py-4"
 >
   <a href="/" class="absolute left-3 lg:left-24">
     <h1 class="whitespace-nowrap border-b-2 border-primary text-3xl font-bold">
@@ -23,7 +23,7 @@
   </ul>
   <div
     on:click={changeScheme}
-    class="absolute right-3 grid transform cursor-pointer place-items-center rounded-full border border-primary bg-gray-800 p-2 duration-150 hover:bg-primary lg:right-24"
+    class="absolute right-3 grid transform cursor-pointer place-items-center rounded-full border border-primary dark:bg-gray-800 bg-gray-200 p-2 duration-150 hover:bg-primary dark:hover:bg-primary lg:right-24"
   >
     <img src={Logo} alt="My logo, a small cartoon of me." class="w-8" />
   </div>
--- src/components/common/button.svelte
+++ src/components/common/button.svelte
@@ -1,4 +1,4 @@
 <button
-  class="block w-40 transform rounded-full bg-gray-800 p-2 text-lg outline-none ring-gray-600 duration-150 hover:bg-primary focus-visible:ring lg:p-4"
+  class="block w-40 transform rounded-full dark:bg-gray-800 bg-gray-200 p-2 text-lg outline-none dark:ring-gray-600 ring-gray-900 duration-150 hover:bg-primary dark:hover:bg-primary focus-visible:ring lg:p-4"
   ><slot /></button
 >
