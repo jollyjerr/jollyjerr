51d10f7d82a202597f4f74facdf37d8d45f63f78
Jeremiah Tabb <jollyjerr@gmail.com>
2022-04-07 17:35:32 -0600

    start from scratch

---
+++ makefile
@@ -0,0 +1,26 @@
+install:
+	pnpm install
+
+dev:
+	pnpm dev
+
+build:
+	pnpm build
+
+package:
+	pnpm package
+
+preview:
+	pnpm preview
+
+prepare:
+	pnpm prepare
+
+typecheck:
+	pnpm typecheck
+
+lint:
+	pnpm lint
+
+format:
+	pnpm format
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,2 @@
-<script>
-  import Page from '../components/page.svelte';
-  import Profile from '../images/profile.jpg';
-</script>
-
-<Page
-  title="About"
-  image={{
-    src: Profile,
-    alt: 'Me',
-  }}
->
-  <h3 class="text-2xl">👋 Welcome!</h3>
-  <p class="text-xl">
-    I am a full-stack engineer with a passion for turning complicated ideas into
-    slick applications.
-  </p>
-  <p class="text-xl">
-    I build solutions using whatever tools necessary. I love writing client
-    facing interfaces with React, Vue and Svelte. For server side work, I love
-    Node.js, Golang, Python and Elixir.
-  </p>
-  <p class="text-xl">
-    I live and work in Boulder, CO. If I am not coding, you can find me climbing
-    rocks all around the US or at home learning a new skill.
-  </p>
-</Page>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
