1c2d11a7d5b3d4744cf58c5acc080c6430126627
Jeremiah Tabb <jollyjerr@gmail.com>
2022-04-10 18:37:49 -0600

    not sure

---
+++ src/lib/landing/landing-page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import MeImg from './me.jpg';
+</script>
+
+<div class="grid grid-cols-2">
+	<div class="bg-background">
+		<h1>Jeremiah Tabb</h1>
+	</div>
+	<img class="from-background bg-gradient-to-r" src={MeImg} alt="me" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
+	import LandingPage from '$lib/landing/landing-page.svelte';
 	import Head from '$lib/seo/components/head.svelte';
 </script>
 
 <Head />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<LandingPage />
