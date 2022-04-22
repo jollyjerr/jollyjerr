764a8759b3bc08fab9096eab0706b49300614327
Jeremiah Tabb <jollyjerr@gmail.com>
2022-04-21 22:08:08 -0600

    desktop navbar

---
+++ src/lib/shared/client/components/navbar-link.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let title: string;
+	export let href: string;
+</script>
+
+<a class="hover:text-primary-2 duration-200 focus-visible:underline" {href}>{title}</a>
--- src/lib/shared/client/components/navbar.svelte
+++ src/lib/shared/client/components/navbar.svelte
@@ -1,3 +1,16 @@
-<nav class="mb-8 lg:text-left text-center">
-	<a href="/" class="text-2xl font-bold">Jeremiah Tabb</a>
+<script lang="ts">
+	import NavbarLink from './navbar-link.svelte';
+
+	export let title: string = 'Jeremiah Tabb';
+</script>
+
+<nav class="mb-8 lg:text-left text-center inline-flex items-end w-full justify-between">
+	<h1 class="text-2xl font-bold focus-visible:underline">{title}</h1>
+	<ul class="space-x-6 hidden lg:block">
+		<NavbarLink title="home" href="/" />
+		<NavbarLink title="blog" href="/blog" />
+		<NavbarLink title="notes" href="/notes" />
+		<NavbarLink title="about" href="/about" />
+		<NavbarLink title="contact" href="/contact" />
+	</ul>
 </nav>
