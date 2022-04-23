46db975c93911f1fc17fe136a24c1ee055e8c304
Jeremiah Tabb <jollyjerr@gmail.com>
2022-04-22 23:52:45 -0600

    fix header

--- src/lib/blog/client/pages/post/post.svelte
+++ src/lib/blog/client/pages/post/post.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import type { Post } from '$lib/blog/common/types';
+	import CoverPhoto from '$lib/shared/client/components/cover-photo.svelte';
 
 	export let post: Post;
 </script>
 
 <div class="space-y-6">
-	<img
-		class="rounded h-32 opacity-80 object-cover w-full max-w-4xl mx-auto"
-		src={post.metadata.image}
-		alt={post.metadata.title}
-	/>
+	<CoverPhoto src={post.metadata.image} alt={post.metadata.title} />
 	<article class="prose prose-invert mx-auto max-w-4xl prose-headings:text-primary-3">
 		{@html post.html}
 	</article>
