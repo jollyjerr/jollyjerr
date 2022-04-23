db2f253e8957ae7d82666d10b800a81b4741202d
Jeremiah Tabb <jollyjerr@gmail.com>
2022-04-22 23:21:21 -0600

    linking and styling

--- src/lib/blog/client/pages/post/post.svelte
+++ src/lib/blog/client/pages/post/post.svelte
@@ -4,7 +4,12 @@
 	export let post: Post;
 </script>
 
-<div>
+<div class="space-y-6">
+	<img
+		class="rounded h-32 opacity-80 object-cover w-full max-w-4xl mx-auto"
+		src={post.metadata.image}
+		alt={post.metadata.title}
+	/>
 	<article class="prose prose-invert mx-auto max-w-4xl prose-headings:text-primary-3">
 		{@html post.html}
 	</article>
--- src/lib/shared/client/components/navbar/navbar.svelte
+++ src/lib/shared/client/components/navbar/navbar.svelte
@@ -4,12 +4,22 @@
 
 	export let title: string = 'Blog';
 	export let selected: 'blog' | 'notes' | 'about' | 'contact' | null;
+
+	const selectedToLink = {
+		blog: '/',
+		notes: '/notes',
+		about: '/about',
+		contact: '/contact'
+	};
 </script>
 
 <nav class="mb-10 lg:text-left text-center inline-flex items-end w-full justify-between">
 	<div class="inline-flex items-end gap-2 pointer-events-none">
 		<img src={IconImg} alt="My emoji - A small 8 bit character" class="w-12" />
-		<h1 class="text-3xl font-bold">{title}</h1>
+		<a
+			href={selected ? selectedToLink[selected] : '/'}
+			class="text-3xl font-bold pointer-events-auto hover:text-primary-2">{title}</a
+		>
 	</div>
 	<ul class="space-x-6 hidden lg:block">
 		<NavbarLink selected={selected === 'blog'} title="blog" href="/" />
