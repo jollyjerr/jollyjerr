7e9cde95707624281ff5d2fe9a98e195d40e9e30
Jeremiah Tabb <jollyjerr@gmail.com>
2022-05-01 21:00:32 -0600

    start form

---
+++ src/lib/contact/contact-page.svelte
@@ -0,0 +1,29 @@
+<form
+	action="https://formsubmit.co/jollyjerr+website@gmail.com"
+	method="POST"
+	class="text-primary-1"
+>
+	<input
+		class="bg-primary-7 rounded focus:outline- border-none focus:ring-2 focus:ring-primary-5"
+		type="text"
+		name="name"
+		placeholder="name"
+		required
+	/>
+	<input
+		class="bg-primary-7 rounded focus:outline- border-none focus:ring-2 focus:ring-primary-5"
+		type="email"
+		name="email"
+		placeholder="email"
+		required
+	/>
+	<textarea
+		class="bg-primary-7 resize-none rounded focus:outline- border-none focus:ring-2 focus:ring-primary-5"
+		name="message"
+		placeholder="message"
+		cols="30"
+		rows="10"
+		required
+	/>
+	<button type="submit">Send</button>
+</form>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,7 +1,9 @@
 <script>
+	import ContactPage from '$lib/contact/contact-page.svelte';
 	import Head from '$lib/seo/components/head.svelte';
 	import Navbar from '$lib/shared/client/components/navbar/navbar.svelte';
 </script>
 
 <Head title="Contact" />
 <Navbar selected="contact" title="Contact" />
+<ContactPage />
