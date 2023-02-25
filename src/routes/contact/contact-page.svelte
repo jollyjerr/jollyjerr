ee9a6d58e65237477c132962d71725d18db3660e
Jeremiah Tabb <jollyjerr@gmail.com>
2023-02-25 12:36:48 -0700

    clean up contact form

--- src/routes/contact/contact-page.svelte
+++ src/routes/contact/contact-page.svelte
@@ -2,10 +2,10 @@
 	import ContactForm from './contact-form.svelte';
 </script>
 
-<div class="lg:space-y-6 space-y-12 grid lg:grid-cols-2 lg:p-8">
-	<div class="space-y-5 lg:text-left text-center">
-		<h2 class="text-5xl">Questions or comments?</h2>
-		<p class="text-2xl">Send me an email here <span class="hidden lg:inline-flex">-></span></p>
+<div class="max-w-lg w-full mx-auto space-y-4 mb-28 lg:mb-0">
+	<div class="text-center bg-primary-7 p-2 rounded">
+		<h2 class="text-2xl">Questions or comments?</h2>
+		<p class="text-lg">Send me an email here</p>
 	</div>
 	<ContactForm />
 </div>
