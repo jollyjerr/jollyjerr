c40a11118126f9d03c51116e0883c89e6a4e18e7
Jeremiah Tabb <jollyjerr@gmail.com>
2023-02-26 18:16:57 -0700

    start work on tag filtering

--- src/routes/tags.svelte
+++ src/routes/tags.svelte
@@ -7,24 +7,39 @@
 		.flatMap((post) => post.tags)
 		.reduce<string[]>((acc, tag) => (acc.includes(tag) ? acc : [...acc, tag]), []);
 
-	$: selectedTags = [] as string[];
-	let filteredPosts = posts.filter((post) => post.tags.every((tag) => selectedTags.includes(tag)));
+	let selectedTags: string[] = [];
+
+	function toggle(tag: string) {
+		if (selectedTags.includes(tag)) {
+			selectedTags = selectedTags.filter((t) => t !== tag);
+		} else {
+			selectedTags = [...selectedTags, tag];
+		}
+	}
+
+	$: filteredPosts = (
+		selectedTags.length > 0
+			? posts.filter((post) => selectedTags.every((tag) => post.tags.includes(tag)))
+			: posts
+	).slice(0, 8);
 </script>
 
 <h1 class="text-2xl font-bold p-6 rounded bg-primary-7">Search blogs by tag</h1>
-<ul class="flex gap-2 my-4">
+<ul class="flex flex-wrap gap-2 my-4">
 	{#each allTags as tag}
 		<li
-			class={`p-4 rounded ${selectedTags.includes(tag) ? 'bg-primary-6' : 'bg-primary-7'}`}
-			on:click={() => {
-				if (selectedTags.includes(tag)) {
-					selectedTags.push(tag);
-				} else {
-					selectedTags = selectedTags.filter((t) => t !== tag);
-				}
-			}}
+			class={`p-4 rounded cursor-pointer hover:ring-1 active:ring-1 ring-primary-4 ${
+				selectedTags.includes(tag) ? 'bg-primary-6' : 'bg-primary-7'
+			}`}
+			on:click={() => toggle(tag)}
+			on:keypress={() => toggle(tag)}
 		>
 			#{tag}
 		</li>
+	{/each}
+</ul>
+<ul>
+	{#each filteredPosts as post}
+		<li>{post.title}</li>
 	{/each}
 </ul>
