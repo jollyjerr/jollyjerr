940215191b5e7a191c8bfb09dbaecd597c139d55
Jeremiah Tabb <jollyjerr@gmail.com>
2023-06-12 18:16:38 -0600

    meh

--- src/lib/navbar/navbar.svelte
+++ src/lib/navbar/navbar.svelte
@@ -12,7 +12,7 @@
 	<div class="pointer-events-none inline-flex items-end gap-2">
 		<img src={IconImg} alt="My emoji - A small 8 bit character" class="z-10 w-12" />
 		<a
-			href={`/${selected}`}
+			href={selected === 'blog' ? '/' : `/${selected}`}
 			class="pointer-events-auto transform text-3xl font-bold duration-200 hover:text-primary-2"
 			>{title}</a
 		>
