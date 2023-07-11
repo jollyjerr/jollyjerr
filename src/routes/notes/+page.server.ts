aa57ee4831d2a54ca89ee0d6cb25b304817a4ccf
Jeremiah Tabb <jollyjerr@gmail.com>
2023-07-10 20:07:58 -0600

    refactor: migrate to notree

--- src/routes/notes/+page.server.ts
+++ src/routes/notes/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from './$types';
 import { pathToNotes } from '$lib/notes/constants';
-import { FileSystem } from '@mindgraph/read';
+import { FileSystem } from '@notree/server';
 
 export const load: PageServerLoad = async () => {
 	const graph = await FileSystem.read({ path: pathToNotes });
