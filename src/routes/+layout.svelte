307e3fa32358db3aeb1e65dcabdc4bbbec184b20
Jeremiah Tabb <jollyjerr@gmail.com>
2023-07-16 10:12:21 -0600

    create base and app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script>
-	import Socials from './socials.svelte';
-	import Algolia from '$lib/algolia/algolia.svelte';
 	import '../app.css';
-	import '../highlight.css';
 </script>
 
 <main class="min-h-screen overflow-x-hidden font-common text-primary-1 antialiased">
-	<Algolia />
-	<div class="container mx-auto px-3 py-3 lg:py-8">
-		<slot />
-	</div>
-	<Socials />
+	<slot />
 </main>
