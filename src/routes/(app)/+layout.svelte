49e56af33e9f5e2259023fa0015934d356208387
Jeremiah Tabb <51095001+jollyjerr@users.noreply.github.com>
2023-07-16 11:03:16 -0600

    feat: linktree (#26)
    * create base and app layout
    
    * update knowledge
    
    * finish linktree

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Socials from './socials.svelte';
+	import Algolia from '$lib/algolia/algolia.svelte';
+	import '../../highlight.css';
+</script>
+
+<Algolia />
+<div class="container mx-auto px-3 py-3 lg:py-8">
+	<slot />
+</div>
+<Socials />
---
+++ src/routes/linktree/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Head from '$lib/head/head.svelte';
+	import { links } from './links.json';
+
+	const image =
+		'https://ik.imagekit.io/jollyjerr/jtabb_dev/IMG_9275_SlROuIxg1.jpg?updatedAt=1689525287506&tr=w-1080%2Ch-1080%2Cfo-auto';
+</script>
+
+<Head title="Links | Jeremiah Tabb" description="My entire online presence" {image} />
+<div class="m-auto flex w-full max-w-xl flex-col items-center space-y-6 px-2 py-12">
+	<img
+		src={image}
+		alt="A happy Jeremiah after climbing J-Crack in Lumpy"
+		style="border-radius: 50%;"
+		class="w-full max-w-[180px]"
+	/>
+	<h1 class="text-2xl font-bold text-primary-5">@jollyjerr</h1>
+	<ul class="w-full space-y-3">
+		{#each links as link}
+			<a
+				class="block w-full rounded bg-primary-7 p-3 text-center text-lg hover:bg-primary-6"
+				target="_blank"
+				href={link.href}
+			>
+				{link.title}
+			</a>
+		{/each}
+	</ul>
+</div>
