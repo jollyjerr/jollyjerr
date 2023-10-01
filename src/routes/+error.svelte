b5ad00056d88d27f7af346b5f41d7e6cd1bfddc3
Jeremiah Tabb <jollyjerr@gmail.com>
2023-09-30 19:06:55 -0600

    feat: better error message

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
 <div class="grid h-screen w-full place-items-center">
 	<div class="space-y-4 text-center">
-		<h1 class="text-xl font-bold">Welcome to the 404 page</h1>
+		<h1 class="text-xl font-bold">Error: {$page?.error?.message || 'iz broke :('}</h1>
 		<a href="/" class="hover:underline">Go Home</a>
 	</div>
 </div>
