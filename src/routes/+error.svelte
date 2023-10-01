f42bbd5063ff942bae68612835d533e831b40a04
Jeremiah Tabb <jollyjerr@gmail.com>
2023-09-30 19:00:57 -0600

    feat: add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<div class="grid h-screen w-full place-items-center">
+	<div class="space-y-4 text-center">
+		<h1 class="text-xl font-bold">Welcome to the 404 page</h1>
+		<a href="/" class="hover:underline">Go Home</a>
+	</div>
+</div>
