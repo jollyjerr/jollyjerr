7e06a595f83bcf8f57f3ca398c7ec1b2247e1d5f
Jeremiah Tabb <jollyjerr@gmail.com>
2025-05-17 23:02:22 -0600

    links page design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,84 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	import { onMount } from 'svelte';
 
 	let { children } = $props();
-	let isDarkMode = $state(false);
-
-	onMount(() => {
-		const savedMode = localStorage.getItem('theme');
-		if (savedMode) {
-			isDarkMode = savedMode === 'dark';
-			updateTheme();
-		} else {
-			isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-			updateTheme();
-		}
-	});
-
-	function toggleTheme() {
-		isDarkMode = !isDarkMode;
-		updateTheme();
-	}
-
-	function updateTheme() {
-		localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-		if (isDarkMode) {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	}
 </script>
 
-<main class="flex min-h-screen flex-col overflow-x-hidden pt-20">
-	<div class="flex-grow">
-		{@render children()}
-	</div>
-	<footer class="p-4">
-		<div class="container mx-auto flex items-center justify-end">
-			<button
-				onclick={toggleTheme}
-				class="flex items-center justify-center rounded-full bg-gray-200
-                                                p-2 text-gray-800
-                                                transition-colors duration-300
-                                                hover:bg-gray-300 dark:bg-gray-700
-                                                dark:text-gray-200 dark:hover:bg-gray-600"
-			>
-				{#if isDarkMode}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M12 3v1m0 16v1m9-9h-1M4 12H3m3.343-5.657L5.636 5.636m12.728 12.728L18.364 18.364M12 7a5 5 0 110 10 5 5 0 010-10z"
-						/>
-					</svg>
-					<span class="ml-2">Light Mode</span>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-						/>
-					</svg>
-					<span class="ml-2">Dark Mode</span>
-				{/if}
-			</button>
-		</div>
-	</footer>
+<main class="min-h-screen overflow-x-hidden pt-20">
+	{@render children()}
 </main>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Head from '$lib/head/head.svelte';
+	import { links } from './links.json';
+
+	const image =
+		'https://ik.imagekit.io/jollyjerr/jtabb_dev/IMG_9275_SlROuIxg1.jpg?updatedAt=1689525287506&tr=w-1080%2Ch-1080%2Cfo-auto';
+</script>
+
+<link rel="preload" href={image} as="image" />
+<Head title="Links | Jer Tabb" description="My entire online presence" {image} />
+<div class="m-auto flex w-full max-w-xl flex-col items-center space-y-6 px-2 py-12">
+	<div class="aspect-square h-[160px] w-[160px]">
+		<img
+			src={image}
+			alt="A happy Jeremiah after climbing J-Crack in Lumpy"
+			class="w-full rounded-full"
+			fetchpriority="high"
+		/>
+	</div>
+	<h1 class="text-3xl font-bold text-neutral-800">@jollyjerr</h1>
+	<ul class="w-full space-y-3">
+		{#each links as link (link.href)}
+			<a
+				class="
+                    block
+                    w-full
+                    rounded-lg
+                    bg-neutral-100
+                    p-3
+                    text-center
+                    text-lg
+                    font-medium
+                    text-neutral-800
+                    shadow-sm
+                    transition-all
+                    duration-100
+                    hover:translate-y-[-4px]
+                    hover:bg-neutral-50
+                    hover:shadow-md"
+				target="_blank"
+				href={link.href}
+			>
+				{link.title}
+			</a>
+		{/each}
+	</ul>
+</div>
