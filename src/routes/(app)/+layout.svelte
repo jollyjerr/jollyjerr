2763aef3336917852de2787242429a132537de37
Jeremiah Tabb <jollyjerr@gmail.com>
2025-05-22 11:39:41 -0600

    fix: scroll bugs
    fixes some scrolling bugs on mobile and keyboard

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
+	import { useSearch } from '$lib/search/api.svelte';
 	import Search from '$lib/search/search.svelte';
 	import '../../highlight.css';
 	import Footer from './footer.svelte';
 
 	let { children } = $props();
+
+	const search = useSearch();
 </script>
 
 <Search />
-<div class="relative flex h-full flex-col">
-	<div class="container mx-auto w-full grow px-4">
-		{@render children()}
-	</div>
-	<Footer />
-</div>
+<main class="container mx-auto grow px-4" class:pt-20={!search.open}>
+	{@render children()}
+</main>
+<Footer />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import { useSearch } from '$lib/search/api.svelte';
 	import '../app.css';
 
 	let { children } = $props();
-
-	const search = useSearch();
 </script>
 
-<main class="h-full overflow-x-hidden" class:pt-20={!search.open}>
-	{@render children()}
-</main>
+{@render children()}
