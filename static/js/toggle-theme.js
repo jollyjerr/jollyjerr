839b8f352919e40fd9b1a610e94934ccd0f38d68
Jeremiah Tabb <jollyjerr@gmail.com>
2025-12-30 11:54:50 -0700

    Fix some lighthouse issues

--- static/js/toggle-theme.js
+++ static/js/toggle-theme.js
@@ -1,19 +1,21 @@
-function toggleTheme() {
-    if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
+document.addEventListener("DOMContentLoaded", () => {
+    function toggleTheme() {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        }
     }
-}
 
-const toggleBtn = document.getElementById("theme-toggle");
-const sun = document.getElementById("sun-icon");
-const moon = document.getElementById("moon-icon");
+    const toggleBtn = document.getElementById("theme-toggle");
+    const sun = document.getElementById("sun-icon");
+    const moon = document.getElementById("moon-icon");
 
-toggleBtn.addEventListener("click", () => {
-    toggleTheme();
-    sun.classList.toggle("hidden");
-    moon.classList.toggle("hidden");
+    toggleBtn.addEventListener("click", () => {
+        toggleTheme();
+        sun.classList.toggle("hidden");
+        moon.classList.toggle("hidden");
+    });
 });
